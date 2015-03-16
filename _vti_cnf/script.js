7be40238bf9cf19d934af0a4965b31acdbe99374
EOnyenezido <sleonvictor@gmail.com>
2015-03-16 16:17:03 +0100

    sample-site

---
+++ _vti_cnf/script.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|HP\\EMEKA
+vti_modifiedby:SR|HP\\EMEKA
+vti_timelastmodified:TW|16 Mar 2015 14:45:01 -0000
+vti_timecreated:TR|16 Mar 2015 08:13:51 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|index.html
+vti_nexttolasttimemodified:TW|16 Mar 2015 14:45:01 -0000
+vti_cacheddtm:TX|16 Mar 2015 14:45:01 -0000
+vti_filesize:IR|317
